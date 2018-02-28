2c8d998f33208fcd9561abd9d3cdbf6d14cfe26e
no_word <zxcvbnm78912@gmail.com>
2018-02-28 20:24:31 +0900

    11720

--- src/bj/bj11720.java
+++ src/bj/bj11720.java
@@ -1,5 +1,23 @@
 package bj;
+import java.util.Scanner;
 
 public class bj11720 {
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		String number= sc.next();
+		int results=0;	
+		
+		for(int i=0; i<n; i++) {
 
+			results+= (int)number.charAt(i)-'0';
+			
+		}
+		System.out.println(results);
+
+		
+		
+		sc.close();
+}
 }

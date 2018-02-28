bfae673c4da5cde8459b0ba333f7c5bfc6b5d10c
no_word <zxcvbnm78912@gmail.com>
2018-02-28 19:11:42 +0900

    Update bj2440.java

--- src/bj/bj2440.java
+++ src/bj/bj2440.java
@@ -4,6 +4,22 @@ public class bj2440 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	int n= sc.nextInt();
+	
+	for(int i=n; i>0; i--) {
+		
+		for(int j=i; j>0; j--) {
+			System.out.print("*");
+		}
+		
+		System.out.println();
+	}
+	
+	sc.close();
+}
 
 	}
 
